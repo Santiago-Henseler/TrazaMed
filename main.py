11fb5064252dcc7ab4a172ab6fb8ea579cea83b5
Santiago-Henseler <116313638+Santiago-Henseler@users.noreply.github.com>
2025-07-08 11:39:26 -0300

    update a pagina actual

--- main.py
+++ main.py
@@ -4,39 +4,29 @@ import sys
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import ttk
+from tkcalendar import DateEntry
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
 
 # Defino constantes
 user = 
 clave = 
 
-fecha_lejana = '01/06/2022'
-
-droguerias = ['DROGUERIA META S A', 'GRUPO SUD LATIN S.A.', 'EQUS FARMA S.R.L.', 'DROGUERIA INSUTRACK S. R. L.','ONCO LIFE S.R.L.','MONROE AMERICANA SOCIEDAD ANONIMA','EFSA S.A.','GLOBAL MED S A','CO FA LO ZA LDA COOPERATIVA DE FARMACIAS DE L DE ZAMORA','ASISTENCIA FARMACEUTICA S A','Todas']
-
-path0 = '/html/body/form/div/table/tbody/'
-path1 = '/html/body/div/table/tbody/tr/td/div/table/tbody/tr[2]/td/div/div/form/table/tbody/tr/td[2]/table/tbody/'
-path2 = '/html/body/div[2]/table/tbody/tr/td/div/form/table/tbody/tr[2]/td/div[1]/'
-
-"""
-class Listado:
-    def __init__(self, id, nComprobante, nAfil, nObra):
-        self.id = id
-        self.nComprobante = nComprobante
-        self.nAfil = nAfil
-        self.nObra = nObra
-"""
+#Inicializo el web driver
+driver = webdriver.Chrome("chromedriver.exe")
 
 def inicio(driver):
     # entramos a la pagina con user y clave
-    driver.get("http://servicios.cofa.org.ar/ncr/")
-    driver.find_element("xpath", f'{path0}tr[1]/td[2]/select/option[9]').click()
-    driver.find_element("xpath", f'{path0}tr[2]/td[2]/input').send_keys(user)
-    driver.find_element("xpath", f'{path0}tr[3]/td[2]/input[1]').send_keys(clave)
-    driver.find_element("xpath", f'{path0}/tr[3]/td[2]/input[2]').click()
+    path0 = '/html/body/div[1]/form/table/tbody/'
+    driver.get("https://principal.cofa.org.ar/")
+    driver.find_element("xpath", f'{path0}tr[1]/td[2]/input').send_keys(user)
+    driver.find_element("xpath", f'{path0}tr[2]/td[2]/input').send_keys(clave)
+    driver.find_element("xpath", f'{path0}tr[3]/td[2]/input[2]').click()
 
     #  entro a la seccion de trazas
-    driver.find_element("xpath", f'{path0}tr[1]/td/div[1]/table[2]/tbody/tr/td[1]/a').click()
+    driver.find_element("xpath", f'/html/body/div[3]/table/tbody/tr[3]/td/a').click()
+    driver.find_element("xpath", f'/html/body/div[3]/table/tbody/tr[3]/td/ul/li[7]').click()
+    
 
 def guardar_datos(driver):
     try:
@@ -54,24 +44,27 @@ def guardar_datos(driver):
 
     archivo.close()
 
-def traer_datos(driver, drogueria):
+def traer_datos(drogeria, drogerias):
 
-    # Selecciono la fecha
-    driver.find_element("xpath",f'{path1}tr[1]/td[2]/p/input').clear()
-    driver.find_element("xpath",f'{path1}tr[1]/td[2]/p/input').send_keys(fecha_lejana)
-    driver.find_element("xpath", f'{path1}tr[2]/td[3]/input').click()
+    index = drogerias.index(drogeria)+1
+    path1 = '/html/body/div/table/tbody/tr/td/div/table/tbody/tr[2]/td/div/div/form/table/tbody/tr/td[2]/table/tbody/'                
+    driver.find_element("xpath", f'{path1}tr[2]/td[2]/select/option[{index}]').click()
     time.sleep(1)
+    driver.find_element("xpath", f'{path1}tr[3]/td[3]/input').click()
 
-    # Trazo todos o algunos
-    if drogueria != 'Todas':
-        driver.find_element("xpath", f'{path1}tr[2]/td[2]/select/option[{droguerias.index(drogueria)+2}]').click()
-        time.sleep(1)
-        driver.find_element("xpath", f'{path1}tr[3]/td[3]/input').click()
-        guardar_datos(driver)
-    else:
-        guardar_datos(driver)
+    guardar_datos(driver)
 
-def trazar(driver):
+def trazar(drogueria, drogerias):
+
+    if forma.get() == "automatico":
+        # Lleno con todas las trazas el archivo
+        traer_datos(drogueria, drogerias)
+        driver.find_element("xpath", '/html/body/div/table/tbody/tr/td/div/table/tbody/tr[2]/td/div/div/div/form/input').click()
+        time.sleep(3)
+
+    # Cambio a la seccion para trazar 
+    driver.find_element("xpath", f'/html/body/div/table/tbody/tr/td/div/table/tbody/tr[1]/td/a[1]').click()
+
     try:
         archivo = open("datos.txt", "r")
     except:
@@ -79,13 +72,13 @@ def trazar(driver):
         sys.exit()
 
     numeros = archivo.readlines()
+    path2 = '/html/body/div[2]/table/tbody/tr/td/div/form/table/tbody/tr[2]/td/div[1]/'
 
     for numero in numeros:
 
         try:
             driver.switch_to.parent_frame()
             driver.switch_to.default_content()
-
             driver.find_element("xpath", f'{path2}table[1]/tbody/tr/td[2]/input').send_keys(numero.split("\n")[0])
             driver.find_element("xpath", f'{path2}table[1]/tbody/tr/td[3]/input').click()
 
@@ -107,53 +100,70 @@ def trazar(driver):
         # Recorremos cada remedio y lo validamos #
             for iframe in iframes:
                 driver.switch_to.parent_frame()
-                driver.switch_to.frame(driver.find_element("xpath", f'{path2}/table[3]/tbody/tr[2]/td[6]/iframe'))
-                driver.execute_script('trazar31()')
-                time.sleep(4)
+                try:
+                    driver.switch_to.frame(driver.find_element("xpath", f'{path2}/table[3]/tbody/tr[2]/td[6]/iframe'))
+                    driver.execute_script('trazar31()')
+                    time.sleep(4)
+                except:
+                    driver.find_element("xpath", f'/html/body/div/div[2]/button[1]').click()
+
             time.sleep(3)
 
         except:
             messagebox.showerror('Error', f'Error al cargar la traza {numero}')
-            archivo.close()
-            sys.exit()
 
+    messagebox.showinfo(title="FIN DE TRAZA", message="Se termino de trazar")
     archivo.close()
-
+    sys.exit()
 def main():
-
-    drogueria = listaDesplegble1.get()
-
-    carga = listaDesplegble2.get()
-
-    #cierro la visual de la app
-    root.quit()
-
-    #Inicializo el web driver
-    driver = webdriver.Chrome("chromedriver.exe")
 
     # Abro la pagina
     inicio(driver)
 
     time.sleep(3)
 
-    # Cambio a la segunda pagina
-    handles = driver.window_handles
-    driver.switch_to.parent_frame()
-    driver.switch_to.default_content()
-    driver.switch_to.window(handles[1])
+    # Seteo la fecha de inicio y fin de la traza
+    path1 = '/html/body/div/table/tbody/tr/td/div/table/tbody/tr[2]/td/div/div/form/table/tbody/tr/td[2]/table/tbody/'
+    fechaIn = calIn.get_date()
+    driver.find_element("xpath", f'{path1}tr[1]/td[2]/p/input').clear()
+    driver.find_element("xpath", f'{path1}tr[1]/td[2]/p/input').send_keys(parse_fecha(fechaIn))
+    fechaFin = calFin.get_date()
+    driver.find_element("xpath", f'{path1}tr[1]/td[4]/p/input').clear()
+    driver.find_element("xpath", f'{path1}tr[1]/td[4]/p/input').send_keys(parse_fecha(fechaFin))
+    driver.find_element("xpath", f'{path1}tr[2]/td[3]/input').click()
 
-    # Lleno con todas las trazas el archivo
-    if carga == 'Automatico':
-        traer_datos(driver, drogueria)
-        driver.find_element("xpath", '/html/body/div/table/tbody/tr/td/div/table/tbody/tr[2]/td/div/div/div/form/input').click()
-        time.sleep(6)
+    # Obtengo las drogerias que puedo trazar
+    time.sleep(1)
+    drogerias = [i.text for i in Select(driver.find_element("xpath", f'{path1}tr[2]/td[2]/select')).options]
+    
+    # Agrego seccion de drogeria a la pestaña
+    canvas2 = tk.Canvas(root, width=400, height=300, relief='raised')
+    canvas2.config(background="#ECF0F1")
+    canvas2.pack()
 
-    time.sleep(2)
+    # box text 3
+    label3 = tk.Label(root, text='Seleccionar drogeria:')
+    label3.config(font=('helvetica', 12), background="#F2F3F4")
+    canvas2.create_window(200, 140, window=label3)
 
-    # Cambio a la seccion para trazar
-    driver.find_element("xpath", f'/html/body/div/table/tbody/tr/td/div/table/tbody/tr[1]/td/a[1]').click()
+    # crear lista desplegable para seleccionar drogeria
+    tipoDrog = ttk.Combobox(
+        state = "readonly",
+        values = drogerias
+    )
+    tipoDrog.config(width=40)
+    canvas2.create_window(200, 165, window=tipoDrog)
 
-    trazar(driver)
+    # boton para trazar
+    button2 = tk.Button(text='Trazar', command=lambda:trazar(tipoDrog.get(), drogerias))
+    button2.config(width=10, background="#D5D8DC")
+    canvas2.create_window(200, 200, window=button2)
+        
+    
+def parse_fecha(fecha_in):
+    fecha = str(fecha_in)
+    fsplit = fecha.split("-")
+    return str(fsplit[2] + "/" + fsplit[1] + "/" + fsplit[0])
 
 
 ####################################################################
@@ -175,34 +185,38 @@ label1 = tk.Label(root, text='Trazabilidad medicamentos cofa')
 label1.config(font=('helvetica', 20), background="#AED6F1", width=60, height=2, pady=1)
 canvas1.create_window(200, 25, window=label1)
 
-# box text 2
-label2 = tk.Label(root, text='Seleccionar drogueria:')
+# box calendario inicio
+label2 = tk.Label(root, text='Seleccionar fecha inicio:')
 label2.config(font=('helvetica', 12), background="#F2F3F4")
-canvas1.create_window(200, 90, window=label2)
+canvas1.create_window(125, 90, window=label2)
 
-# crear lista desplegable con las dif droguerias
-listaDesplegble1 = ttk.Combobox(
-    state = "readonly",
-    values = droguerias
-)
-listaDesplegble1.config(width=40)
-canvas1.create_window(200, 130, window=listaDesplegble1)
+calIn = DateEntry(root, date_pattern="dd/mm/yyyy")
+calIn.pack()
+canvas1.create_window(275, 90, window=calIn)
 
-# box text 3
-label2 = tk.Label(root, text='Cargar datos:')
+# box calendario fin
+label2 = tk.Label(root, text='Seleccionar fecha final:')
 label2.config(font=('helvetica', 12), background="#F2F3F4")
-canvas1.create_window(200, 165, window=label2)
+canvas1.create_window(125, 120, window=label2)
 
-# crear lista desplegable para saber si los datos son cargados manuales o no
-listaDesplegble2 = ttk.Combobox(
+calFin = DateEntry(root, date_pattern="dd/mm/yyyy")
+calFin.pack()
+canvas1.create_window(275, 120, window=calFin)
+
+# box text 4
+label4 = tk.Label(root, text='Seleccionar forma:')
+label4.config(font=('helvetica', 12), background="#F2F3F4")
+canvas1.create_window(125, 150, window=label4)
+
+# crear lista desplegable para seleccionar drogeria
+forma = ttk.Combobox(
     state = "readonly",
-    values = ['Manual', 'Automatico']
+    values = ["manual", "automatico"]
 )
-listaDesplegble2.config(width=40)
-canvas1.create_window(200, 195, window=listaDesplegble2)
+canvas1.create_window(275, 150, window=forma)
 
-# boton para trazar
-button1 = tk.Button(text='Trazar', command=main)
+# boton para empezar trazar
+button1 = tk.Button(text='Empezar trazar', command=main)
 button1.config(width=10, background="#D5D8DC")
 canvas1.create_window(200, 265, window=button1)
 
